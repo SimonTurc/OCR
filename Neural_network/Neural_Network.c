e9f56a1795f37d37ab035315503cebe61a845079
Sturc <s.turc241@gmail.com>
2020-10-29 17:25:56 +0100

    Change Makefile

--- Neural_network/Neural_Network.c
+++ Neural_network/Neural_Network.c
@@ -1,9 +1,31 @@
 #include "backprop.h"
 #include "layer.h"
 #include "neuron.h"
-#include "layer.c"
-#include "neuron.c"
 
+layer create_layer(int number_of_neurons)
+{
+	layer lay;
+	lay.num_neu = -1;
+	lay.neu = (struct neuron_t *) malloc(number_of_neurons * sizeof(struct neuron_t));
+	return lay;
+}
+
+neuron create_neuron(int num_out_weights)
+{
+	neuron neu;
+
+	neu.actv = 0.0;
+	neu.out_weights = (float*) malloc(num_out_weights * sizeof(float));
+	neu.bias=0.0;
+	neu.z = 0.0;
+
+	neu.dactv = 0.0;
+	neu.dw = (float*) malloc(num_out_weights * sizeof(float));
+	neu.dbias = 0.0;
+	neu.dz = 0.0;
+
+	return neu;
+}
 
 layer *lay = NULL;
 int num_layers;
@@ -379,6 +401,7 @@ void test_nn(void)
         forward_prop();
     }
 }
+
 
 int dinit(void)
 {
