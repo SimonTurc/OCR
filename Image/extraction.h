a64b0fbff76c52e556521ac2b239bb2f738cc559
Simon TURC <simonturc@pop-os.localdomain>
2020-12-03 13:18:44 +0100

    header fix

--- Image/extraction.h
+++ Image/extraction.h
@@ -1,7 +1,7 @@
 #ifndef EXTRACTION_H
 #define EXTRACTION_H
 
-void extraction(SDL_Surface* image, int char_nb);
+char extraction(SDL_Surface* image, int char_nb);
 int get_length_col(SDL_Surface* image, int width,int col);
 double* fill_matrix(SDL_Surface* image, int start_col, int end_col, double* m);
 int count_char(SDL_Surface* image);
